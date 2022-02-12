536bfe01a9eaa72e7b767c66e97778a0429db530
antiquesince99 <ry663164@gmail.com>
2022-02-12 18:11:17 +0530

    ..

--- frontend/src/component/main/footer.jsx
+++ frontend/src/component/main/footer.jsx
@@ -1,9 +1,28 @@
+import { Box, Container, Typography, Button } from '@mui/material';
 import React from 'react';
+// import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 // import { NavLink } from 'react-router-dom';
+import RedditIcon from '@mui/icons-material/Reddit';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = () => {
   return (
-    <> MainFooter</>
+    <Container style={{display: 'flex', justifyContent:"space-between", padding: "25px" }}> 
+    <Box><Typography variant="h5" color="initial">LOCATION</Typography>
+    <div style={{display: 'flex', justifyContent:"center", flexDirection:"column" , marginTop:"20px"}}>Some Where in India </div>
+    </Box>
+    <Box>
+    <Typography variant="h5" color="initial">AROUND THE WEB</Typography>
+    <div style={{display: 'flex', justifyContent:"space-around", marginTop: "20px"}}>
+    <YouTubeIcon></YouTubeIcon>
+    <FacebookIcon></FacebookIcon>
+    <RedditIcon></RedditIcon>
+    </div>
+  
+    </Box>
+    <Box><Typography variant="h5" color="initial">ABOUT</Typography></Box>
+    </Container>
   )
 };
 
