2d8689d52d5df42c934cfeeb906934160f31374a
antiquesince99 <ry663164@gmail.com>
2022-02-26 14:15:49 +0530

    ..

--- frontend/src/component/main/signup.jsx
+++ frontend/src/component/main/signup.jsx
@@ -1,14 +1,15 @@
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import React from "react";
 import { Formik } from "formik";
 import app_config from "../../config";
 import Swal from "sweetalert2";
+import { Box } from "@mui/system";
+
 const Signup = () => {
   const url = app_config.api_url;
   const Registerform = {
     fullname: "",
     email: "",
-    username: "",
     password: "",
     created: new Date(),
   };
@@ -36,47 +37,82 @@ const Signup = () => {
   };
   return (
     <div>
-      <Formik initialValues={Registerform} onSubmit={formSubmit}>
-        {({ values, handleSubmit, handleChange }) => (
-          <form onSubmit={handleSubmit}>
-            <input
-              placeholder="FullName"
-              className="form-control"
-              id="fullname"
-              onChange={handleChange}
-              value={values.fullname}
-            />
+      <Container maxWidth="lg">
+        <Box
+          style={{
+            margin: "5rem",
+            padding: "2rem",
+            display: "flex",
+            height: "50vh",
+          }}
+        >
+          <Formik initialValues={Registerform} onSubmit={formSubmit}>
+            {({ values, handleSubmit, handleChange }) => (
+              <form onSubmit={handleSubmit} style={{display:"flex" , flexDirection:"column" , justifyContent: "center" , alignItems:"center" , margin:"auto"}}>
+                <input
+                  placeholder="FullName"
+                  className="form-control"
+                  id="fullname"
+                  onChange={handleChange}
+                  value={values.fullname}
+                  style={{
+                    marginBottom:"2rem",
+                    height:"50px",
+                    padding:"8px",
+                    borderRadius:"10px",
+                    fontSize:"15px",
 
-            <input
-              placeholder="Email"
-              className="form-control"
-              id="email"
-              onChange={handleChange}
-              value={values.email}
-            />
+                  }}
+                />
 
-            <input
-              placeholder="UserName"
-              className="form-control"
-              id="username"
-              onChange={handleChange}
-              value={values.username}
-            />
+                <input
+                  placeholder="Email"
+                  className="form-control"
+                  id="email"
+                  onChange={handleChange}
+                  value={values.email}
+                  style={{
+                    marginBottom:"2rem",
+                    height:"50px",
+                    padding:"8px",
+                    borderRadius:"10px",
+                    fontSize:"15px",
 
-            <input
-              className="form-control"
-              placeholder="*******"
-              id="password"
-              autoComplete="off"
-              onChange={handleChange}
-              value={values.password}
-            />
-            <Button variant="contained" color="success" type="submit">
-              register
-            </Button>
-          </form>
-        )}
-      </Formik>
+                  }}
+                />
+
+                <input
+                  className="form-control"
+                  placeholder="password"
+                  id="password"
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={values.password}
+                  style={{
+                    marginBottom:"2rem",
+                    height:"50px",
+                    padding:"8px",
+                    borderRadius:"10px",
+                    fontSize:"15px",
+
+                  }}
+                />
+                <Button variant="contained" color="success" type="submit"
+                  style={{
+                    marginBottom:"2rem",
+                    height:"50px",
+                    padding:"8px",
+                    borderRadius:"10px",
+                    fontSize:"15px",
+                    cursor: "pointer"
+                  }}>
+                  register
+                </Button>
+              </form>
+            )}
+          </Formik>
+        </Box>
+      </Container>
     </div>
   );
 };
